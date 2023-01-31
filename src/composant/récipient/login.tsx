35854174ba966ff9257b8c0d35f19ed50ee1dec0
Weber <71732780+petuniahtf@users.noreply.github.com>
2023-01-31 16:50:55 +0100

    Add testMobil

---
+++ src/composant/récipient/login.tsx
@@ -0,0 +1,14 @@
+import React from react
+
+
+type LoginComponentProps = {
+ children : React.reactNode
+} 
+
+const LoginComponent = ({children}:LoginComponentProps) =>{
+Return (
+<div>{children} </div>
+) 
+}
+
+export default LoginComponent;
